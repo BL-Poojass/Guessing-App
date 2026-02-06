1811b9e0c8743d25367a04087039aed0b7268ea6
BL-Poojass <pooja.ss@bridgelabz.com>
2026-02-06 16:17:57 +0530

    UC1: Start the guessing game

---
+++ src/com/guessingapp1/GuessingGame.java
@@ -0,0 +1,9 @@
+package com.guessingapp1;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Guessing Game!");
+        System.out.println("Game Started...");
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Guessing_App {
+}
