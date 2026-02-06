c06b234ca1e1945d29cd7f80050882eb106b67fa
BL-Poojass <pooja.ss@bridgelabz.com>
2026-02-06 16:23:01 +0530

    UC2: Generate random number

--- src/com/guessingapp1/GuessingGame.java
+++ src/com/guessingapp1/GuessingGame.java
@@ -1,9 +1,16 @@
 package com.guessingapp1;
 
+import java.util.Random;
+
 public class GuessingGame {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Guessing Game!");
-        System.out.println("Game Started...");
+
+        Random random = new Random();
+        int targetNumber = random.nextInt(100) + 1;
+
+        System.out.println("Random number generated. Let's play!");
     }
 }
