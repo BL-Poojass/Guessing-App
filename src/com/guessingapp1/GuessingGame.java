7efe0c208662406b05a2c53de881ac5bcb2ac227
BL-Poojass <pooja.ss@bridgelabz.com>
2026-02-06 16:52:24 +0530

    UC5: Provide high or low hints until correct guess

--- src/com/guessingapp1/GuessingGame.java
+++ src/com/guessingapp1/GuessingGame.java
@@ -1,9 +1,33 @@
 package com.guessingapp1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessingGame {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Guessing Game!");
-        System.out.println("Game Started...");
+
+        Random random = new Random();
+        int targetNumber = random.nextInt(100) + 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your guess: ");
+        int userGuess = scanner.nextInt();
+
+        while (userGuess != targetNumber) {
+
+            if (userGuess < targetNumber) {
+                System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("Too high! Try again.");
+            }
+
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+        }
+
+        System.out.println("Congratulations! You guessed the number.");
     }
 }
